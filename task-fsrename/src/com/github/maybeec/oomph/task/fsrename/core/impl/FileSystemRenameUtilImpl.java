1aa5e1a047157ba3305615945bc4b030cc1fdbba
Steffen Holzer <steffen.holzer@capgemini.com>
2016-08-16 17:33:44 +0200

    fixes #1 added additional log output in case of errors

--- task-fsrename/src/com/github/maybeec/oomph/task/fsrename/core/impl/FileSystemRenameUtilImpl.java
+++ task-fsrename/src/com/github/maybeec/oomph/task/fsrename/core/impl/FileSystemRenameUtilImpl.java
@@ -9,36 +9,41 @@ import com.github.maybeec.oomph.task.fsrename.core.exception.FSRenameException;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
 
 /**
  * @author sholzer
  *
  */
-public class FileSystemRenameUtilImpl implements FileSystemRenameUtil
-{
+public class FileSystemRenameUtilImpl implements FileSystemRenameUtil {
 
-  /*
-   * (non-Javadoc)
-   * @see com.github.maybeec.oomph.task.fsrename.core.FileSystemRenameUtil#rename(java.lang.String, java.lang.String)
-   */
-  public void rename(String source, String destination) throws FSRenameException
-  {
-    SetupTaskLogger LOG = SetupTaskLogger.getLogger();
-    LOG.logInfo("Renaming " + source + " to " + destination);
-    Path sourcePath = Paths.get(source);
-    Path destinationPath = Paths.get(sourcePath.getParent().toString() + File.separator + destination);
-    try
-    {
-      Files.move(sourcePath, destinationPath);
-    }
-    catch (IOException ex)
-    {
-      throw new FSRenameException(ex.getMessage());
-    }
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.github.maybeec.oomph.task.fsrename.core.FileSystemRenameUtil#rename(
+	 * java.lang.String, java.lang.String)
+	 */
+	public void rename(String source, String destination) throws FSRenameException {
+		SetupTaskLogger LOG = SetupTaskLogger.getLogger();
+		LOG.logInfo("Renaming " + source + " to " + destination);
+		Path sourcePath = Paths.get(source);
+		Path destinationPath = Paths.get(sourcePath.getParent().toString() + File.separator + destination);
+		try {
+			Files.move(sourcePath, destinationPath);
+		} catch (IOException ex) {
+			for(StackTraceElement el :  ex.getStackTrace()){
+				LOG.logError(el.toString());
+			}
+			
+			throw new FSRenameException(ex.getMessage());
+		}
 
-  }
+	}
 
 }
